f748b185744972f0212fa032ff4cf17ad69b9512
Sorayukii <sorayukii69@gmail.com>
2026-04-02 18:25:57 +0700

    mm: Add Kconfig option for sysctl knobs for protecting the working set
    Signed-off-by: Sorayukii <sorayukii69@gmail.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -1829,6 +1829,7 @@ static struct ctl_table vm_table[] = {
 		.extra2		= &one,
 	},
 #endif
+#ifdef CONFIG_WORKINGSET_PROTECTION
 	{
 		.procname	= "workingset_protection",
 		.data		= &sysctl_workingset_protection,
@@ -1876,6 +1877,7 @@ static struct ctl_table vm_table[] = {
 		.extra1		= &zero,
 		.extra2		= &one_hundred,
 	},
+#endif
 	{
 		.procname	= "user_reserve_kbytes",
 		.data		= &sysctl_user_reserve_kbytes,
